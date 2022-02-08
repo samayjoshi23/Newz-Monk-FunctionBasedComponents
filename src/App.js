f6ec7545edebb9493e8f57ea68fe7d234c71bcf0
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-02-08 14:24:44 +0530

    First commit (clone for class based comonents)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+
+
+export default class App extends Component {
+
+  pageSize = 6;
+  apiKey = '67094f40903c4e0ab0f73d415277b578'
+  // apiKey = process.env.REACT_APP_NEWS_API
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar/>
+          <LoadingBar 
+            color='rgb(237, 9, 25)' 
+            progress={this.state.progress} 
+            height={5} 
+            shadow={true} 
+            loaderSpeed={1000}
+            />
+          
+          <Routes>
+            <Route exact path="/" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="general"/>} />
+            <Route exact path="/sports" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country="in" category="sports"/>} />
+            <Route exact path="/science" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country="in" category="science"/>} />
+            <Route exact path="/health" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country="in" category="health"/>} />
+            <Route exact path="/technology" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country="in" category="technology"/>} />
+            <Route exact path="/business" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="in" category="business"/>} />
+            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+
+  render() {
+
+    let {title, description, imageUrl,newsUrl,author,date,source} = this.props;
+
+    return (
+      <div className='mt-4'>
+        <div className="card">
+            <img src={imageUrl} className="card-img-top article-img" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">
+                  {title}
+                  <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger px-3"
+                        style={{left:'50%', zIndex: '10'}}
+                  >
+                    {source}
+                  </span>
+                </h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toUTCString()}</small></p>
+                <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
